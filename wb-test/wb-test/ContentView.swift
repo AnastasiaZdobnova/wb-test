7f0decbde7e635e8f3e06e31ef079ff89f8f4f22
AnastasiaZdobnova <113519201+AnastasiaZdobnova@users.noreply.github.com>
2023-09-25 15:22:56 +0600

    added a post request and data decryption

--- wb-test/wb-test/ContentView.swift
+++ wb-test/wb-test/ContentView.swift
@@ -1,24 +1,55 @@
-//
-//  ContentView.swift
-//  wb-test
-//
-//  Created by Анастасия Здобнова on 25.09.2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .onAppear {
+                    fetchData()
+                }
         }
         .padding()
     }
-}
-
-#Preview {
-    ContentView()
+    
+    @State private var flights: [Flight] = [] // Свойство для хранения данных о рейсах
+    
+    func fetchData() {
+        if let url = URL(string: "https://vmeste.wildberries.ru/stream/api/avia-service/v1/suggests/getCheap") {
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("https://vmeste.wildberries.ru", forHTTPHeaderField: "Origin")
+            request.addValue("https://vmeste.wildberries.ru/avia", forHTTPHeaderField: "Referer")
+            
+            let requestBody = ["startLocationCode": "LED"]
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: requestBody, options: [])
+                request.httpBody = jsonData
+            } catch {
+                print("Ошибка при создании JSON-данных: \(error)")
+            }
+            
+            URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if let error = error {
+                    print("Ошибка запроса: \(error)")
+                } else if let data = data {
+                    do {
+                        let decoder = JSONDecoder()
+                        let flightResponse = try decoder.decode(FlightResponse.self, from: data)
+                        DispatchQueue.main.async {
+                            self.flights = flightResponse.flights // Сохраняем данные о рейсах
+                            print("Полученные рейсы: \(self.flights)")
+                        }
+                        // Теперь у вас есть массив данных о рейсах в свойстве flights
+                    } catch {
+                        print("Ошибка при декодировании JSON: \(error)")
+                    }
+                }
+            }.resume()
+        }
+    }
 }
---
+++ wb-test/wb-test/FlightData.swift
@@ -0,0 +1,31 @@
+//
+//  FlightData.swift
+//  wb-test
+//
+//  Created by Анастасия Здобнова on 25.09.2023.
+//
+
+import Foundation
+
+struct Seat: Codable {
+    let count: Int
+    let passengerType: String
+}
+
+struct Flight: Codable {
+    let endCity: String
+    let endDate: String
+    let endLocationCode: String
+    let price: Int
+    let searchToken: String
+    let seats: [Seat]
+    let serviceClass: String
+    let startCity: String
+    let startDate: String
+    let startLocationCode: String
+}
+
+struct FlightResponse: Codable {
+    let flights: [Flight]
+}
+
