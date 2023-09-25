3cb89734bd49094eff68fad94c8999a29b30e005
AnastasiaZdobnova <113519201+AnastasiaZdobnova@users.noreply.github.com>
2023-09-25 14:30:46 +0600

    initial commit

---
+++ wb-test/wb-test/wb_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  wb_testApp.swift
+//  wb-test
+//
+//  Created by Анастасия Здобнова on 25.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct wb_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
